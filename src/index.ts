09f649690552ebab4c676a4252f9cf65742337a0
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2024-08-25 21:22:19 -0400

    name and description was changed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import {getEasyDate, getEasyTime, getEasyFormat, getEasyDateTimeStructure} from './modules/easy-datetime'
-
-const today = new Date('2024-08-04T19:09:03')
-console.log(getEasyDateTimeStructure(today))
-console.log(getEasyFormat(today, "ESP"))
-console.log(getEasyDate(today, "long", "ESP"))
-
-
+import {
+  getEasyDate,
+  getEasyDateTimeStructure,
+  getEasyFormat,
+  getEasyTime,
+} from "./modules/easy-datetime";
